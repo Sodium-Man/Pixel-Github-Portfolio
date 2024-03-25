0d52b25747b147b06ff3e0e77282dc2e80619df4
NamaN Bhalani <bhalaninaman007@gmail.com>
2024-03-25 11:44:45 +0530

    Content Update

--- src/constants.js
+++ src/constants.js
@@ -3,19 +3,27 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've also made a game in Java language similar to this type. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+    Here is my <a href="https://github.com/Sodium-Man" target="_blank">Github</a>!`,
+
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+
   "sofa-table": `That's my sofa. I like to relax here and watch Taarak Mehta Ka Ooltah Chashmah. 
   As a connoisseur of web series and TV shows, I possess an unparalleled passion for captivating storytelling and a discerning eye for selecting only the most exquisite GOD Tier productions, making me your ultimate guide to the world of premium entertainment.`,
+
   tv: `That's my TV. I've been watching Documentaries alot recently like :
    <a href="https://tv.apple.com/us/movie/shoah-second-era/umc.cmc.3vzc5d6byuiux0w4xfrnm66p9?playableId=tvs.sbd.9001%3A1550241597" target="_blank">Shoah</a>, <a href="https://www.netflix.com/in/title/80091741?source=35" target="_blank">13th</a>,
-  <a href="https://www.amazon.com/Lake-Fire-Noam-Chomsky/dp/B07W5ZZYXQ" target="_blank">Lake of Fire</a> (sometimes) and <a href="https://www.netflix.com/in/title/70300800" target="_blank">Bojack Horseman</a>!`,
+  <a href="https://www.amazon.com/Lake-Fire-Noam-Chomsky/dp/B07W5ZZYXQ" target="_blank">Lake of Fire</a> and currently the reason for my melancholy is <a href="https://www.netflix.com/in/title/70300800" target="_blank">Bojack Horseman</a>!`,
+
   bed: `This is where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at bhalaninaman7@gmail.com if you have any good suggestions for Dramas and ofcourse Job Opportunities ;)!`,
+
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/drive/u/0/folders/1ppLQ6VayhLDqjT0ZB2s5--Em_qhP4YMQ" target="_blank">Check it out?</a>
+  Contact me at bhalaninaman7@gmail.com if you have any good suggestions for Dramas and ofcourse Job Opportunities ;)`,
+
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+
   library: `There are a lot of programming books on my shelves. There is even one in French and German (I also speak French and German btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+
   exit: `If you want to exit NamaN's portfolio, just close the tab.`,
 };
